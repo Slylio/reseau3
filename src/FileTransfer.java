9ccf21d3bd701917d1b3bcec1d952aaadebd0049
Slylio <slylio.videogamer@gmail.com>
2022-03-26 15:16:16 +0100

    meilleur code

--- src/FileTransfer.java
+++ src/FileTransfer.java
@@ -2,72 +2,64 @@ import java.net.*;
 import java.io.*;
 
 public class FileTransfer{
+    private String hostname;
+    private int socketNum;
+    private String filename;
+    
+    //cas ou serveur en attente     
+    public FileTransfer(String hostname, int socketNum){
+        this.hostname=hostname;
+        this.socketNum=socketNum;
+    }
+    //cas de client qui envoie le fichier
+    private FileTransfer(String hostname, int socketNum, String filename){
+        this.hostname=hostname;
+        this.socketNum=socketNum;
+        this.filename=filename;
+    }
 
     public static void main(String[] args){
         if (args.length==2){
-            Server server = new Server(args[0],Integer.parseInt(args[1]));
+            FileTransfer fileTransfer = new FileTransfer(args[0],Integer.parseInt(args[1]));
             try {
-                server.run();
+                fileTransfer.runServer();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else if (args.length==3){
-            Client client = new Client(args[0],Integer.parseInt(args[1]), args[2]);
+            FileTransfer fileTransfer = new FileTransfer(args[0],Integer.parseInt(args[1]), args[2]);
             try {
-                client.run();
+                fileTransfer.runClient();
             } catch (Exception e) {
                 System.err.println("les paramètres attendus sont : hostname, numéro de socket, nom du fichier qu'on veut lire");
             }
         }
     }
-    
-    
-    private class Client{
-        private String hostname;
-        private int socketNum;
-        private String filename;
-
-        private Client(String hostname, int socketNum, String filename){
-            this.hostname=hostname;
-            this.socketNum=socketNum;
-            this.filename=filename;
-        }
-
-        private void run() throws UnknownHostException, IOException{
-            Socket client = new Socket(hostname,socketNum);
-            System.out.println("Connexion au serveur...");
-            try (InputStream in = new FileInputStream(new File(filename))){
-                byte[] data= new byte[1024];
-                int l= in.read(data,0,1024);
-                OutputStream out = client.getOutputStream();
-                while (l!= -1) {
-                    out.write(data,0,l);
-                    l= in.read(data,0,1024);
-                }
-                out.close();
-                in.close();
-            } catch (IOException e){
-                System.err.println(e);
-            }
+    //méthode en commun 
+    private void transfer(InputStream in, OutputStream out) throws IOException{
+        byte data[] = new byte[1024];   
+        int l;
+        while((l=in.read(data))!=-1){   //tant qu'il y a de la data (indicateur = -1)
+            out.write(data,0,l);    //on écrit dans le output
         }
     }
 
-    private class Server{
-        private String hostname;
-        private int socketNum;
-        private Server(String hostname, int socketNum){
-            this.hostname=hostname;
-            this.socketNum=socketNum;
-        }
-
-        private void run() throws IOException{
-            ServerSocket fileServer = new ServerSocket(socketNum);
-            System.out.println("Serveur lancé sur le port : "+ socketNum);
-            System.out.println("Ecoute...");
-            Socket socketClient = fileServer.accept();
-            OutputStream out = 
-            System.out.println("Connexion : "+socketClient.getInetAddress());
-        }
+    //méthode run pour le client
+    private void runClient() throws UnknownHostException, IOException{
+        Socket sock = new Socket(hostname,socketNum);
+        //on prend un fichier en entrée et on renvoit la data
+        transfer(new FileInputStream(filename),sock.getOutputStream());
+        sock.close();
+    }
+        
+    //méthode run pour le serveur
+    private void runServer() throws UnknownHostException, IOException{
+        try (Socket sock = new ServerSocket(socketNum).accept()) {  //try with ressource
+            //on prend les data du client en entrée et on renvoie un file
+            transfer(sock.getInputStream(),new FileOutputStream(filename)); 
+            sock.close();
+        } 
     }
 }
+
 
